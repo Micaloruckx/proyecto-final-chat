358dd2e89f3b9515d26c9cdf082aacd9920956de
micaloruckx <micaelalorusso@hotmail.com>
2026-02-22 11:06:01 -0300

    Se añadió el Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
 
 function App() {
 
 
   return (
     <div>
-      
+      <nav>      <h1>clon.wpp</h1>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h2>Home</h2>} />
+        <Route path="/chats" element={<h2>Chats</h2>} />
+      </Routes>
+      <Route path="/login" element={<h2>Login</h2>} />
     </div>
   )
 }
